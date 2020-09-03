af41ab4a3f9bf57fcc78d845923ef44d7123a889
jorgetinto <jpinogaray@gmail.com>
2020-09-03 18:27:37 -0400

    fin seccion 15

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SidebarService } from '../../services/sidebar.service';
+import { UsuarioService } from 'src/app/services/usuario.service';
+import { Usuario } from 'src/app/models/usuario.model';
 
 @Component({
   selector: 'app-sidebar',
@@ -9,11 +11,12 @@ import { SidebarService } from '../../services/sidebar.service';
 })
 export class SidebarComponent implements OnInit {
 
-  menuItems: any[];
+  public menuItems: any[];
+  public usuario: Usuario;
 
-  constructor( private sidebarService: SidebarService ) {
-    this.menuItems = sidebarService.menu;
-    console.log(this.menuItems)
+  constructor( private sidebarService: SidebarService, private usuarioService: UsuarioService ) {
+    this.menuItems   = sidebarService.menu;
+    this.usuario     = usuarioService.usuario;
   }
 
   ngOnInit(): void {
